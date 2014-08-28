f1e8249175000e360dc6ecdc8fc998f7273f7b8b
Liraz Siri <liraz@turnkeylinux.org>
2014-08-28 18:20:30 +0300

    finished implementing google-calendar-dump

--- google-calendar-dump.py
+++ google-calendar-dump.py
@@ -13,9 +13,6 @@ Options:
     --creds=PATH        Where to load/save OAuth2 credentials
                         Defaults: ~/.config/google-calendar-dump.dat
 
-    --max-results=NUM   Set maximum number of results to return
-                        Defaults to None (uses API default)
-
 Example usage:
 
     google-calendar-dump myemail@gmail.com 2014-1-1
@@ -28,65 +25,14 @@ from os.path import *
 import sys
 import getopt
 
+import pprint
+import dateutil.parser
+import datetime
+
 from fixedmap import FixedMap
 
 # credentials for consumer app, not end-user
 
-class API:
-    CLIENT_ID = '202930974095-vds3f7krnef053ibmhojl2ivsqcg3n41.apps.googleusercontent.com'
-    CLIENT_SECRET = 'urPmLmos5dw6wekTMfZV2O0z'
-
-    HOME_CREDSFILE = '.config/google-calendar-dump.dat'
-
-    @classmethod
-    def _get_credentials(cls, credsfile=None):
-        """get credentials stored in credsfile, or get credentials via OAuth and store in PATH"""
-
-        from oauth2client.client import OAuth2WebServerFlow
-        from oauth2client import tools
-        from oauth2client.file import Storage
-
-        if credsfile is None:
-            credsfile = join(os.environ['HOME'], cls.HOME_CREDSFILE)
-
-        flow = OAuth2WebServerFlow(client_id=cls.CLIENT_ID,
-                                   client_secret=cls.CLIENT_SECRET,
-                                   scope='https://www.googleapis.com/auth/calendar')
-
-        storage = Storage(credsfile)
-        credentials = storage.get()
-
-        # workaround because we don't use argparse
-        class cmd_flags:
-            def __init__(self):
-                self.short_url = True 
-                self.noauth_local_webserver = False
-                self.logging_level = 'ERROR' 
-                self.auth_host_name = 'localhost'
-                self.auth_host_port = [8080, 9090]
-
-        if credentials is None or credentials.invalid == True:
-            credentials = tools.run_flow(flow, storage, flags=cmd_flags())
-
-        return credentials
-
-    @classmethod
-    def get_service(cls, credsfile=None):
-        """return service API"""
-
-        import httplib2
-        from apiclient.discovery import build
-
-        # Create an httplib2.Http object to handle our HTTP requests and authorize it
-        # with our good Credentials.
-        credentials = cls._get_credentials(credsfile)
-
-        http = httplib2.Http()
-        http = credentials.authorize(http)
-        service = build(serviceName='calendar', version='v3', http=http)
-
-        return service
-    
 class Map(FixedMap):
     """Like Map except it allows missing values in kws"""
 
@@ -98,6 +44,60 @@ class Map(FixedMap):
         FixedMap.__init__(self, **defaults)
 
 class Calendars:
+    class API:
+        CLIENT_ID = '202930974095-vds3f7krnef053ibmhojl2ivsqcg3n41.apps.googleusercontent.com'
+        CLIENT_SECRET = 'urPmLmos5dw6wekTMfZV2O0z'
+
+        HOME_CREDSFILE = '.config/google-calendar-dump.dat'
+
+        @classmethod
+        def _get_credentials(cls, credsfile=None):
+            """get credentials stored in credsfile, or get credentials via OAuth and store in PATH"""
+
+            from oauth2client.client import OAuth2WebServerFlow
+            from oauth2client import tools
+            from oauth2client.file import Storage
+
+            if credsfile is None:
+                credsfile = join(os.environ['HOME'], cls.HOME_CREDSFILE)
+
+            flow = OAuth2WebServerFlow(client_id=cls.CLIENT_ID,
+                                       client_secret=cls.CLIENT_SECRET,
+                                       scope='https://www.googleapis.com/auth/calendar')
+
+            storage = Storage(credsfile)
+            credentials = storage.get()
+
+            # workaround because we don't use argparse
+            class cmd_flags:
+                def __init__(self):
+                    self.short_url = True 
+                    self.noauth_local_webserver = False
+                    self.logging_level = 'ERROR' 
+                    self.auth_host_name = 'localhost'
+                    self.auth_host_port = [8080, 9090]
+
+            if credentials is None or credentials.invalid == True:
+                credentials = tools.run_flow(flow, storage, flags=cmd_flags())
+
+            return credentials
+
+        @classmethod
+        def get_service(cls, credsfile=None):
+            """return service API"""
+
+            import httplib2
+            from apiclient.discovery import build
+
+            # Create an httplib2.Http object to handle our HTTP requests and authorize it
+            # with our good Credentials.
+            credentials = cls._get_credentials(credsfile)
+
+            http = httplib2.Http()
+            http = credentials.authorize(http)
+            service = build(serviceName='calendar', version='v3', http=http)
+
+            return service
 
     class Calendar(Map):
         FIELDS = ['id', 'summary', 'description', 'accessRole']
@@ -110,7 +110,7 @@ class Calendars:
             FixedMap.__init__(self, *args, **kws)
 
     def __init__(self, credsfile=None):
-        self.service = API.get_service(credsfile)
+        self.service = self.API.get_service(credsfile)
 
     def iter_calendars(self):
 
@@ -137,7 +137,44 @@ class Calendars:
     @property
     def calendars(self):
         return list(self.iter_calendars())
-    
+
+    @staticmethod
+    def fmt_values(keywords):
+        keywords = keywords.copy()
+
+        for k, v in keywords.items():
+            if v is None:
+                del keywords[k]
+
+            if isinstance(v, datetime.date):
+                keywords[k] = v.isoformat() + 'T00:00:00Z'
+
+        return keywords
+
+    def iter_events(self, calendar_id, **kws):
+        events = self.service.events()
+        kws = self.fmt_values(kws)
+
+        if 'maxResults' not in kws:
+            kws['maxResults'] = 2500
+
+        nextpage = None
+        while True:
+            request = events.list(calendarId=calendar_id, 
+                                  pageToken=nextpage,
+                                  **kws)
+
+            response = request.execute()
+
+            items = response.get('items')
+            if items:
+                for event in response['items']:
+                    yield event
+
+            nextpage = response.get('nextPageToken')
+            if not nextpage:
+                break
+
 def usage(e=None):
     if e:
         print >> sys.stderr, "error: " + str(e)
@@ -153,35 +190,18 @@ def fatal(s):
     print >> sys.stderr, "error: " + str(s)
     sys.exit(1)
 
-def _dump_calendar(calendar, credsfile, max_results=None):
-    
-
-    service = get_service(credsfile)
-    events = service.events()
-
-    request = events.list(
-        calendarId='liraz.siri2@gmail.com', 
-        timeMin='2014-08-21T00:00:00Z',
-        timeMax='2014-08-22T00:00:00Z',
-        singleEvents=True, 
-        orderBy='startTime', 
-        fields='nextPageToken,items(colorId,start,end,start,summary)')
-
-    print request.execute()
-
-def dump_calendar(calendar, credsfile, since=None, until=None, max_results=None):
-    print `(calendar, credsfile, since, until, max_results)`
-
 def parse_date(s):
-    pass
+    return dateutil.parser.parse(s).date()
+
+def fmt_date(date):
+    return date.isoformat() + "T00:00:00Z"
 
 def main():
-    max_results = None
     credsfile = None
 
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], 'h', 
-                                       ['list-calendars', 'creds=', 'max-results='])
+                                       ['list-calendars', 'creds='])
     except getopt.GetoptError, e:
         usage(e)
 
@@ -196,12 +216,6 @@ def main():
                 fatal("credentials file '%s' does not exist" % val)
 
             credsfile = val
-
-        elif opt == '--max-results':
-            try:
-                max_results = int(val)
-            except ValueError:
-                fatal("illegal max-results value '%s'" % val)
 
         if opt == '--list-calendars':
             opt_list_calendars = True
@@ -218,8 +232,9 @@ def main():
     if len(args) < 1:
         usage()
 
-    calendar = args[0]
+    calendar_id = args[0]
 
+    since = until = None
     try:
         if len(args) > 1:
             since = parse_date(args[1])
@@ -230,8 +245,13 @@ def main():
     except Error, e:
         fatal(e)
 
-    api = API(credsfile)
-    dump_calendar(api, calendar, since=since, until=until, max_results=max_results)
+    kws = {
+        'timeMin': since,
+        'timeMax': until
+    }
+    for event in Calendars(credsfile).iter_events(calendar_id, **kws):
+        print
+        pprint.pprint(event)
 
 if __name__=="__main__":
     main()
