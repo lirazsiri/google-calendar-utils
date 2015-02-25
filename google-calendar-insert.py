91896372202dc3adcd287208f15907359198963d
Liraz Siri <liraz@turnkeylinux.org>
2015-02-25 19:57:58 +0200

    google-calendar-insert optimization: dump events within inserted time range

--- google-calendar-insert.py
+++ google-calendar-insert.py
@@ -31,6 +31,9 @@ import getopt
 from api import Calendars
 from utils import lines2records
 
+import datetime
+import dateutil.parser
+
 import simplejson
 
 def usage(e=None):
@@ -82,10 +85,7 @@ def parse_cli_args(args):
 
     return options, args
 
-def calendar_get_all_events(cal, calendar_id):
-    all_events = list(cal.iter_events(calendar_id, singleEvents=True,
-                                                   showDeleted=False,
-                                                   orderBy='startTime'))
+def calendar_get_all_events(cal, calendar_id, timeMin, timeMax):
 
     return all_events
 
@@ -138,6 +138,22 @@ def filter_duplicates(all_events, events):
 def parse_input(fh):
     return [ simplejson.loads(record) for record in lines2records(fh) ]
 
+def getstartdate(event):
+    event_start = event.get('start')
+    if not event_start:
+        return None
+
+    time = event_start.get('dateTime', event_start.get('date'))
+    return dateutil.parser.parse(time).date()
+
+def get_events_timerange(events):
+    dates = [ getstartdate(event) for event in events if getstartdate(event) ]
+    date_min = min(dates)
+    date_max = max(dates)
+
+    oneday = datetime.timedelta(days=1)
+    return (date_min - oneday, date_max + oneday)
+
 def main():
     options, args = parse_cli_args(sys.argv[1:])
 
@@ -145,17 +161,24 @@ def main():
         usage()
 
     calendar_id = args[0]
+
     inserted_events = parse_input(sys.stdin)
+    timeMin, timeMax = get_events_timerange(inserted_events)
 
     cal = Calendars(options.credsfile)
-    all_events = calendar_get_all_events(cal, calendar_id)
+    all_events = list(cal.iter_events(calendar_id,
+                                      timeMin=timeMin,
+                                      timeMax=timeMax,
+                                      singleEvents=True,
+                                      showDeleted=False,
+                                      orderBy='startTime'))
 
-    inserted_events = filter_duplicates(all_events, inserted_events)
+    filtered_events = filter_duplicates(all_events, inserted_events)
     if options.simulate:
-        for event in inserted_events:
+        for event in filtered_events:
             print simplejson.dumps(event, indent=True)
     else:
-        for event in cal.insert_events(calendar_id, filter_duplicates(all_events, inserted_events)):
+        for event in cal.insert_events(calendar_id, filter_duplicates(all_events, filtered_events)):
             print simplejson.dumps(event, indent=True)
 
 if __name__=="__main__":
